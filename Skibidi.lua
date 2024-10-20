74b9eb60e9cd6f10f40e2c9c3b86b06031085e36
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-10-20 20:49:23 +0800

    Create Skibidi.lua

---
+++ Skibidi.lua
@@ -0,0 +1,27 @@
+getgenv.wat = true
+
+local{(
+    local print("ishowfuxkvexril")
+    local do
+      do
+        do
+          do
+            do
+              do
+                do
+                  do
+                    then
+                  end
+                  end 
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+end
+end
+end
+skid.mula = true
