439e01514c13d23eaf279e7f0ee04e8d88582c71
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-10-21 19:57:37 +0800

    Update Skibidi.lua

--- Skibidi.lua
+++ Skibidi.lua
@@ -1,27 +1,74 @@
-getgenv.wat = true
+local Fluxus_Z_loader = true
+local Synapse_loader = true
+local Delta_loader = true
+local Codex_loader = false
 
-local{(
-    local print("ishowfuxkvexril")
-    local do
-      do
+if fluxus_Z_loader = true 
+    then do
+        print("fluxus&hwid")
+    else
         do
-          do
-            do
-              do
-                do
-                  do
-                    then
-                  end
-                  end 
-              end
-            end
-          end
+            print("fluxus&hwidtrue")
         end
-      end
     end
-  end
 end
+else
+if Synapse_loader = true 
+    then do
+        print("Synapse&hwid")
+    else
+        do
+            print("Synapse&hwidtrue")
+        end
+    end
 end
+else
+if Delta_loader = true 
+    then do
+        print("Delta&hwid")
+    else
+        do
+            print("Deltahwidtrue")
+        end
+    end
 end
+else
+if Codex_loader = true 
+    then do
+        print("Codex&hwid")
+    else
+        do
+            print("Codexhwidtrue")
+        end
+    end
 end
-skid.mula = true
+
+local request = request or http_request or Krnl.request or syn.request or Fluxus.request
+
+local function fetchScript(nga)
+    local url = nga
+    local headers = {
+        ["ngrok-skip-browser-warning"] = "true"
+    }
+
+    local response = request({
+        Url = url,
+        Method = "GET",
+        Headers = headers
+    })
+    if not response then
+        
+        return nil
+    end
+    local success, script = pcall(function()
+        return loadstring(response.Body)()
+    end)
+    if not success then
+        
+        return nil
+    end
+    return script
+end
+
+wait(100)
+print("debug.net.for.executer.loaded")
